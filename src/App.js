7c9c7b32619ee9e7c7aba105681e583a4a87a75e
Adeel Hamza <hsarwar234@gmail.com>
2023-06-26 12:53:37 +0500

    mycommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
+import Addwatermark from './components/about/Addwatermark';
+import DrawerAppBar from './components/appbar/Appbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DrawerAppBar/>
+      <Home/>
+      <About/>
+      <Addwatermark/>
     </div>
   );
 }
